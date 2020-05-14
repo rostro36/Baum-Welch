15fc218d2aa00f1a06e20d6577612b3805fbf0b3
Jannik <jgut@student.ethz.ch>
2020-05-14 22:11:59 +0200

    new File layout

--- tested.h
+++
@@ -1,6 +0,0 @@
-#ifndef test_H_   /* Include guard */
-#define test_H_
-
-void tested_implementation(int hiddenStates, int differentObservables, int T, double* transitionMatrix, double* emissionMatrix, double* stateProb, int* observations);  
-
-#endif // test_H_
